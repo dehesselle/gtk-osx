d25b555d6afd1eba29340a3a2627ed7a577c3cfa
René de Hesselle <dehesselle@web.de>
2020-08-04 23:22:47 +0200

    Use Python 3.7
    This version is present on Catalina w/ latest Xcode.

--- gtk-osx-setup.sh
+++ gtk-osx-setup.sh
@@ -148,7 +148,7 @@ meson = {version="==0.51.1"}
 jhbuild = "$DEVPREFIX/libexec/run_jhbuild.py"
 
 [requires]
-python_version = "3.6"
+python_version = "3.7"
 EOF
     cat <<EOF > "$DEVPREFIX/etc/pipenv-env"
 export PYTHONUSERBASE="$PYTHONUSERBASE"
